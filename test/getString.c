6a1c90fec6701e7447a48677d4ebfdee1899bd94
luc <luc@sietium.com>
2020-07-27 12:02:38 +0800

    fix: missing glfwInit()

--- test/getString.c
+++ test/getString.c
@@ -3,6 +3,7 @@
 //
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
@@ -11,9 +12,20 @@ int main(int argc, char **argv)
    char *vendor;
 
    /** Context Initialization */
+   glfwInitHint(GLFW_COCOA_MENUBAR, GLFW_FALSE);
+
+   if (!glfwInit())
+      exit(EXIT_FAILURE);
+
    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-   GLFWwindow *os_ctx = glfwCreateWindow(640, 480, "", NULL, NULL);
-   glfwMakeContextCurrent(os_ctx);
+   GLFWwindow *os_win = glfwCreateWindow(640, 480, "", NULL, NULL);
+   if (!os_win)
+   {
+      glfwTerminate();
+      exit(EXIT_FAILURE);
+   }
+
+   glfwMakeContextCurrent(os_win);
 
    vendor = glGetString(GL_VENDOR);
 
